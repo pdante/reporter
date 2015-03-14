6c780f026c9580de129f1695ce2bb29227e9c211
csemiao <csemiao@gmail.com>
2015-03-14 03:05:56 -0700

    yay

---
+++ src/main/java/reporter/Problem.java
@@ -0,0 +1,17 @@
+package reporter;
+
+/**
+ * Created by Chris on 15-03-14.
+ */
+public class Problem {
+
+    private boolean fixed;
+    Room room;
+
+    Public Problem(Room room) {
+        this.room = room;
+        fixed = false;
+    }
+
+
+}
