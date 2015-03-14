5ea27b8b88d0cb722c334e0a3739384db0d63a2e
csemiao <csemiao@gmail.com>
2015-03-14 03:28:05 -0700

    Fields initialized, basic getters and setters, constructors done.

--- src/main/java/reporter/Room.java
+++ src/main/java/reporter/Room.java
@@ -4,5 +4,32 @@ package reporter;
  * Created by Chris on 15-03-14.
  */
 public class Room {
-    Private String roomNumber;
+    private String roomNumber;
+    private Building building;
+
+    public Room(String roomNumber, Building building) {
+        this.roomNumber = roomNumber;
+        this.building = building;
+    }
+
+    // GETTERS
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public Building getBuilding() {
+        return building;
+    }
+
+    // SETTERS
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public void setBuilding(Building building) {
+        this.building = building;
+    }
+
 }
